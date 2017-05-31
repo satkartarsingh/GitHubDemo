36f12dfd9492077178526351d141e077b75c1f00
satkartarsingh <satkartar.jiit@gmail.com>
2017-05-31 17:11:46 +0530

    First Commit

---
+++ GitHubDemo/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		displayname();
+	}
+
+	public static void displayname()
+	{
+		System.out.println("Welcome to GitHub Demo Program");
+		System.out.println("Learn to Connect Git and Eclipse");
+	}
+
+	
+}
